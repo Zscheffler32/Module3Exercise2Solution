9d01901ea31579ad36b0b71f60e74c62340f6bc1
zscheff <zscheff@2343-vNet-1-0>
2023-05-21 23:20:46 -0500

    Add project files.

---
+++ Module3Exercise2Project/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Module3Exercise2Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRed = new System.Windows.Forms.Button();
+            this.btnBlue = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnRed
+            // 
+            this.btnRed.BackColor = System.Drawing.Color.Red;
+            this.btnRed.ForeColor = System.Drawing.Color.Red;
+            this.btnRed.Location = new System.Drawing.Point(21, 16);
+            this.btnRed.Name = "btnRed";
+            this.btnRed.Size = new System.Drawing.Size(93, 45);
+            this.btnRed.TabIndex = 0;
+            this.btnRed.Text = "R&ED";
+            this.btnRed.UseVisualStyleBackColor = false;
+            this.btnRed.Click += new System.EventHandler(this.btnRed_Click);
+            // 
+            // btnBlue
+            // 
+            this.btnBlue.BackColor = System.Drawing.Color.Blue;
+            this.btnBlue.ForeColor = System.Drawing.Color.Blue;
+            this.btnBlue.Location = new System.Drawing.Point(376, 16);
+            this.btnBlue.Name = "btnBlue";
+            this.btnBlue.Size = new System.Drawing.Size(93, 45);
+            this.btnBlue.TabIndex = 1;
+            this.btnBlue.Text = "&BLUE";
+            this.btnBlue.UseVisualStyleBackColor = false;
+            this.btnBlue.Click += new System.EventHandler(this.btnBlue_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(376, 155);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(93, 45);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "&R";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btnR_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(21, 155);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(93, 45);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "&L";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.btnL_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(137, 234);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(215, 97);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Exit";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Yellow;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(137, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(215, 184);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.button5;
+            this.ClientSize = new System.Drawing.Size(481, 343);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnBlue);
+            this.Controls.Add(this.btnRed);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRed;
+        private System.Windows.Forms.Button btnBlue;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Module3Exercise2Project/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Module3Exercise2Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnR_Click(object sender, EventArgs e)
+        {
+            label1.Text = button3.Text;
+            //changed label from lblMessage to label1
+            //changed to button3.text
+            //hotkey is alt-r
+        }
+
+        private void btnRed_Click(object sender, EventArgs e)
+        {
+            label1.BackColor = btnRed.BackColor;
+            //changed lblMessage to label1
+            //due to conflict with 'r' hotkey is alt-e
+            //text is in the button but same color as background
+        }
+
+        private void btnBlue_Click(object sender, EventArgs e)
+        {
+            label1.BackColor = btnBlue.BackColor;
+            //changed lblMessage to label1
+            //changed btnBlue name in the properties from button1 to btnBlue
+            //hotkey is alt-b
+            //text is in the button but same color as background
+
+        }
+
+        private void btnL_Click(object sender, EventArgs e)
+        {
+            label1.Text = button4.Text;
+            //changed lblMessage to label1
+            //hotkey is alt-l
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            //also added esc function to form
+        }
+
+       
+    }
+}
